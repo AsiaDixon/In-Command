4414ebe3b44e58b4abde1735f8b85137f01f9e5f
AsiaDixon <35752460+AsiaDixon@users.noreply.github.com>
2018-02-27 18:25:47 -0800

    inclusion of folders and other component pages

---
+++ buttons.php
@@ -0,0 +1,7 @@
+<?php include 'partials/start.php';?>
+      <h1 class="text-5xl text-teal font-sans">Buttons </h1>
+<?php include 'partials/c-buttons.php';?>
+<pre><code class="html"><?php echo htmlspecialchars(file_get_contents('partials/c-buttons.php'), ENT_QUOTES); ?>
+  
+</code></pre>
+<?php include 'partials/end.php';?>
--- partials/start.php
+++ partials/start.php
@@ -1,10 +1,3 @@
-<?php
-  $name="Asia";
-
-  $num1=2;
-  $num2=6;
-  $sum= $num1 + $num2;
-?> 
 <!doctype html>
 <html lang="en">
   <head>
@@ -12,7 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="./dist/styles.css">
+    <link rel="stylesheet"
+      href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
   </head>
   <body>
     <?php include 'header.php'; ?>
+    <div class="flex -mx-2">
+      <div class="w-1/3 bg-grey-light px-2">
+<?php include 'navigation.php'; ?>
+      </div>
+    <div class="w-2/3 px-2">
     
